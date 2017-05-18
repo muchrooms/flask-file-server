8cb1ae9e902ed405c8c9b952042b8900d4937753
letli <liletian@shidaixin.com>
2017-05-18 21:11:10 +0800

    add password

--- file_server.py
+++ file_server.py
@@ -96,7 +96,36 @@ def get_range(request):
     else:
         return 0, None
 
+
+from functools import wraps
+from flask import request, Response
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'admin'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
 class PathView(MethodView):
+    @requires_auth
     def get(self, p=''):
         hide_dotfile = request.args.get('hide-dotfile', request.cookies.get('hide-dotfile', 'no'))
 
@@ -135,6 +164,7 @@ class PathView(MethodView):
             res = make_response('Not found', 404)
         return res
 
+    @requires_auth
     def post(self, p=''):
         path = os.path.join(root, p)
         info = {}
